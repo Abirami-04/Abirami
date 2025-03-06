18bfbdd4d38a8042b4a4cd955a2ac2335dbdae3d
Abirami-04 <abirami.cs22@krct.ac.in>
2025-03-06 10:51:12 +0530

    new commit

---
+++ final.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+class final{
+     public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int ab[]=new int[n];
+        for(int i=0;i<n;i++){
+        ab[i]=sc.nextInt();
+        }
+
+    
+         Arrays.sort(ab);
+         System.out.println(ab[0]"min");
+         System.out.println(ab[n-1]);
+        }
+     }
